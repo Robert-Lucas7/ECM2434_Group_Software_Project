ca7eee889fd44bb1e62b8d79e7d071fd1435a784
Elliot Kent <ek506@exeter.ac.uk>
2024-02-13 16:54:02 +0000

    Added template structure

--- sustainability_project/project/views.py
+++ sustainability_project/project/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'project/index.html')
+
+def home(request):
+    return render(request, 'project/home.html')
