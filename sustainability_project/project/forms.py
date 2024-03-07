fad2171826136f2726c9867e7356a4abe1f1d975
Henry Forrest <henryforrest@Henrys-MacBook-Pro.local>
2024-03-07 16:12:38 +0000

    added uni email verification

--- sustainability_project/project/forms.py
+++ sustainability_project/project/forms.py
@@ -19,9 +19,18 @@ class Signup(UserCreationForm):
         model = CustomUser
         fields = ['username', 'email', 'password1', 'password2']
 
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if not email.endswith('@exeter.ac.uk'):
+            raise forms.ValidationError('You must have an exeter univerity email to register')
+        return email
+
 
 class MakePost(forms.Form):
     comment = forms.CharField(
         max_length=200, 
         widget=forms.Textarea(attrs={'style': 'width: 300px; height: 100px;'})
     )
+
+
+
