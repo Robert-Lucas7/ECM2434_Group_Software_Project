d6fa1b63a7a76c64018a6c8f4e4ecf8f02640905
Robert-Lucas7 <rob576821@gmail.com>
2024-03-18 21:49:59 +0000

    Added village shop functionality

--- sustainability_project/sustainability_project/settings.py
+++ sustainability_project/sustainability_project/settings.py
@@ -43,7 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_crontab'
+    'django_crontab',
+    'project.templatetags'
 ]
 
 MIDDLEWARE = [
